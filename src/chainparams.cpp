ff4c2181b590bca489eb33d16bdf03ef862725fa
umbracoindevs <45613717+Umbracoindevs@users.noreply.github.com>
2018-12-11 18:53:35 -0500

    updates

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -140,7 +140,7 @@ public:
         genesis.nNonce = 2459082;
 
 	hashGenesisBlock = genesis.GetHash();
-        assert(hashGenesisBlock == uint256("0x"));
+        assert(hashGenesisBlock == uint256("0x21ba1da9e71c5763cc9385eb71e07e7dd7ddb85320a5bfd722d5a45272ed2aea"));
         assert(genesis.hashMerkleRoot == uint256("0xc61fbf01328779c84b18ca5ffd6dd77b"));
 
         vSeeds.push_back(CDNSSeedData("45.63.76.241", "45.63.76.241"));
