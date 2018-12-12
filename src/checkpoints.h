e1751d26e28ed789fa39942bd4968a337a4865e2
Umbra Coin Devs Team <45613717+Umbracoindevs@users.noreply.github.com>
2018-12-12 08:51:03 -0500

    fixed

--- src/checkpoints.h
+++ src/checkpoints.h
@@ -35,7 +35,7 @@ int GetTotalBlocksEstimate();
 //! Returns last CBlockIndex* in mapBlockIndex that is a checkpoint
 CBlockIndex* GetLastCheckpoint();
 
-double GuessVerificationProgress(CBlockIndex* pindex, bool fSigchecks = false);
+double GuessVerificationProgress(CBlockIndex* pindex, bool fSigchecks = true);
 
 extern bool fEnabled;
 
