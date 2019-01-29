3033bf2b65ee647d97a1b823865fbac6c7a9b3b8
umbracoindevs <45613717+Umbracoindevs@users.noreply.github.com>
2019-01-29 02:05:39 -0500

    testnet ports

--- contrib/seeds/generate-seeds.py
+++ contrib/seeds/generate-seeds.py
@@ -130,7 +130,7 @@ def main():
         process_nodes(g, f, 'pnSeed6_main', 18878)
     g.write('\n')
     with open(os.path.join(indir,'nodes_test.txt'),'r') as f:
-        process_nodes(g, f, 'pnSeed6_test', 18879)
+        process_nodes(g, f, 'pnSeed6_test', 18880)
     g.write('#endif // BITCOIN_CHAINPARAMSSEEDS_H\n')
 
 if __name__ == '__main__':
